9740623572e37672de014d910705e7bb5c2fb346
Lin Lan <lanlin@meideng.net>
2020-11-26 00:11:24 +0800

    一次请求

---
+++ caller/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+)
+
+type ConnWorker struct {
+	conn net.Conn
+	r    []byte
+	w    []byte
+}
+
+var ConnPool = sync.Pool{New: func() interface{} {
+	return &ConnWorker{r: make([]byte, 256), w: make([]byte, 0, 256)}
+}}
+
+func OneCall(address string, action byte, param interface{}) (resp []byte) {
+	worker := ConnPool.Get().(*ConnWorker)
+	defer ConnPool.Put(worker)
+
+	for retry := 0; retry < 1; retry++ {
+		if worker.conn == nil {
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
+			worker.conn = conn
+		}
+
+		var conn, r, w = worker.conn, worker.r, worker.w
+		// 构造请求体
+		// 第1位：消息总长度。第2位：动作。第3~位：参数json
+		js, err := json.Marshal(param)
+		if err != nil {
+			log.Println(err)
+		}
+		w = make([]byte, 0, 256)
+		w = append(w, byte(len(js)), action)
+		w = append(w, js...)
+		conn.Write(w)
+		w = w[:0]
+
+		// 读取响应体
+		// 第1位：长度。第2~位：结果json
+		_, err = conn.Read(r[:1])
+		if err != nil {
+			fmt.Println(err)
+			conn.Close()
+			conn = nil
+			continue
+		}
+		n := int(r[0])
+		_, err = conn.Read(r[:n])
+		if err != nil {
+			fmt.Println(err)
+			conn.Close()
+			conn = nil
+			continue
+		}
+		return r[:n]
+	}
+	return nil
+}
+
+func main() {
+	resp := OneCall("localhost:8080", 1, ActionParam1{3, 6})
+	fmt.Println("返回值：", string(resp))
+}
+
+type ActionParam1 struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
---
+++ worker/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net"
+	"os"
+)
+
+var logger = log.New(os.Stdout, "", log.Llongfile)
+
+func main() {
+	server, _ := net.Listen("tcp", ":8080")
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			logger.Fatalln(err)
+		}
+		go serve(conn)
+	}
+}
+
+func serve(conn net.Conn) {
+	logger.Println("连接：", conn.RemoteAddr())
+	defer conn.Close()
+	r := make([]byte, 256)
+	w := make([]byte, 0, 256)
+	for {
+		// 解析请求体
+		// 第1位：消息总长度。第2位：动作。第3~位：参数json
+		if _, err := conn.Read(r[:2]); err != nil {
+			logger.Println(err)
+			return
+		}
+		var n, action = int(r[0]), r[1]
+		if _, err := conn.Read(r[:n]); err != nil {
+			logger.Println(err)
+			return
+		}
+		// 执行服务端任务函数
+		var js []byte
+		js, err := process(action, r[:n])
+		if err != nil {
+			logger.Println(err)
+			return
+		}
+		// 写回响应体
+		w = w[:1]
+		w[0] = byte(len(js))
+		w = append(w, js...)
+		if _, err := conn.Write(w); err != nil {
+			logger.Println(err)
+			return
+		}
+	}
+}
+
+func process(action byte, req []byte) ([]byte, error) {
+	switch action {
+	case 1:
+		return processAction1(req)
+	default:
+		return nil, errors.New("不支持的action")
+	}
+}
+
+type ActionParam1 struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
+type ActionResult1 struct {
+	Sum int `json:"sum"`
+}
+
+func processAction1(req []byte) ([]byte, error) {
+	var param ActionParam1
+	err := json.Unmarshal(req, &param)
+	if err != nil {
+		return nil, err
+	}
+
+	// 构造响应体
+	// 第1位：长度。第2~位：结果json
+	var res ActionResult1
+	res.Sum = param.A + param.B
+	js, _ := json.Marshal(&res)
+	return js, nil
+}
